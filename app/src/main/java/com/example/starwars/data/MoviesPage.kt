be38c5cd5e4b6ea9347fcbc6f83a57cbde65246d
gusmanmatheus <matheusenrik@hotmail.com>
2023-03-17 18:09:32 -0300

    create layout

--- app/src/main/java/com/example/starwars/data/MoviesPage.kt
+++ app/src/main/java/com/example/starwars/data/MoviesPage.kt
@@ -4,5 +4,5 @@ import com.google.gson.annotations.SerializedName
 
 data class MoviesPage(
     @SerializedName("results")
-    val movieList:List<MoviesPage>
+    val movieList:List<MoviesRemote>
 )
---
+++ app/src/main/java/com/example/starwars/presentation/ListSelectorFragment.kt
@@ -0,0 +1,26 @@
+package com.example.starwars.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.starwars.R
+import com.example.starwars.databinding.FragmentListSelectorBinding
+
+class ListSelectorFragment : Fragment() {
+    private lateinit var binding: FragmentListSelectorBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentListSelectorBinding.inflate(inflater)
+        return binding.root
+    }
+
+}
